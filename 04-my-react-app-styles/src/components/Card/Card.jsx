b646187f17f3c5b53cba7228d151e227b0a47f28
eduardo vinagre <eduardovinagre@gmail.com>
2025-08-15 01:02:54 -0600

    using inline styles

--- 04-my-react-app-styles/src/components/Card/Card.jsx
+++ 04-my-react-app-styles/src/components/Card/Card.jsx
@@ -2,8 +2,16 @@
 import styles from "./Card.module.scss"
 
 const Card = ()=> {
+    const textStyles = {
+        color: "blue", 
+        fontSize: "2rem", 
+        textAlign:"center"
+    }
     return (
-        <div className={styles.card}>
+        // <div className={styles.card}>
+        // Soy una tarjeta!
+        // </div>
+        <div style={textStyles}>
         Soy una tarjeta!
         </div>
     )
